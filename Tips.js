51bd4d9cb1f186cabcbeb9845696fabdb063f096
Victoria-Paglia <68866074+Victoria-Paglia@users.noreply.github.com>
2020-07-29 13:56:00 -0300

    Create Tips.js

---
+++ Tips.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded',
+
+    function showInformation() {
+
+        let tips= document.createElement ("p");
+        document.body.appendChild(tips);
+        tips.id= "Tips";
+
+        tips.innerHTML= "Tips cuidados por coronavirus"
+    })
